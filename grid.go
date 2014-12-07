37f6c74f044e7f2287db5aa1fca5c5ad5713a6ac
Piotr S. Staszewski <p.staszewski@gmail.com>
2014-12-07 21:15:01 +0100

    Add grid generator

---
+++ grid.go
@@ -0,0 +1,73 @@
+// Grid generates grid-based icons.
+
+package ricons
+
+import (
+	"image"
+	"image/color"
+	"image/draw"
+	"math/rand"
+	"time"
+)
+
+type gridIconGen struct {
+	r *rand.Rand
+	s int
+	p []color.Color
+	b image.Image
+}
+
+func (g *gridIconGen) String() string {
+	return "grid: grid-based icons"
+}
+
+func (g *gridIconGen) NewIcon(width, height int) (*Icon, error) {
+	i := NewIcon(width, height)
+	draw.Draw(i.Image, image.Rect(0, 0, width, height), g.b, image.ZP, draw.Src)
+
+	gr := make([][]bool, g.s)
+	el := make([]bool, g.s*g.s)
+	for i := range gr {
+		gr[i] = el[i*g.s : (i+1)*g.s]
+	}
+
+	for i := 0; i < 8; i++ {
+		x := g.r.Intn(g.s)
+		y := g.r.Intn(g.s)
+		gr[x][y] = true
+		if g.r.Float32() > 0.5 {
+			gr[g.s-x-1][y] = true
+		}
+		if g.r.Float32() > 0.5 {
+			gr[x][g.s-y-1] = true
+		}
+	}
+
+	c := image.NewUniform(g.p[g.r.Intn(len(g.p))])
+	w := width / g.s
+	h := height / g.s
+	for y := 0; y < g.s; y++ {
+		for x := 0; x < g.s; x++ {
+			if gr[x][y] {
+				r := image.Rect(x*w, y*h, (x+1)*w, (y+1)*h)
+				draw.Draw(i.Image, r, c, image.ZP, draw.Src)
+			}
+		}
+	}
+
+	return i, nil
+}
+
+func init() {
+	g := &gridIconGen{
+		rand.New(rand.NewSource(time.Now().Unix())),
+		5,
+		[]color.Color{
+			color.RGBA{0xaa, 0x66, 0x66, 0xff},
+			color.RGBA{0x66, 0xaa, 0x66, 0xff},
+			color.RGBA{0x66, 0x66, 0xaa, 0xff},
+		},
+		image.NewUniform(color.RGBA{0xdd, 0xdd, 0xdd, 0xff}),
+	}
+	Register("grid", g)
+}
---
+++ grid_test.go
@@ -0,0 +1,45 @@
+// Tests the basics of the framework on a concrete Generator.
+
+package ricons
+
+import "testing"
+
+func TestGridPresent(t *testing.T) {
+	if gen, exist := Registry["grid"]; !exist {
+		t.Fatal("couldn't find 'grid' generator")
+	} else {
+		g = gen
+	}
+}
+
+func TestGridStringer(t *testing.T) {
+	if g.String() != "grid: grid-based icons" {
+		t.Errorf("stringer mismatch: %v != %v", "grid: grid-based icons", g.String())
+	}
+}
+
+func BenchmarkGrid16x16(b *testing.B) {
+	g, ok := Registry["grid"]
+	if !ok {
+		b.Fatal("couldn't find 'grid' generator")
+	}
+	for i := 0; i < b.N; i++ {
+		_, err := g.NewIcon(16, 16)
+		if err != nil {
+			b.Fatalf("(%d) error generating icon: %s", i+1, err)
+		}
+	}
+}
+
+func BenchmarkGrid32x32(b *testing.B) {
+	g, ok := Registry["grid"]
+	if !ok {
+		b.Fatal("couldn't find 'grid' generator")
+	}
+	for i := 0; i < b.N; i++ {
+		_, err := g.NewIcon(32, 32)
+		if err != nil {
+			b.Fatalf("(%d) error generating icon: %s", i+1, err)
+		}
+	}
+}
