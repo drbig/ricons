81f24f7d25680ccc062cb0810a88bd8c8f941643
Piotr S. Staszewski <p.staszewski@gmail.com>
2014-12-08 09:39:35 +0100

    Add symsquare generator

---
+++ symsquare.go
@@ -0,0 +1,65 @@
+// Square-based symmetric icons.
+
+package ricons
+
+import (
+	"image"
+	"image/color"
+	"image/draw"
+	"math/rand"
+	"time"
+)
+
+type symsquareIconGen struct {
+	r *rand.Rand
+	s int
+	p []color.Color
+	b image.Image
+}
+
+func (g *symsquareIconGen) String() string {
+	return "symsquare: symmetric square-based icons"
+}
+
+func (g *symsquareIconGen) NewIcon(width, height int) (*Icon, error) {
+	i := NewIcon(width, height)
+	draw.Draw(i.Image, image.Rect(0, 0, width, height), g.b, image.ZP, draw.Src)
+
+	cx := width / 2
+	cy := height / 2
+	c := image.NewUniform(g.p[g.r.Intn(len(g.p))])
+
+	for cnt := 0; cnt < 8; cnt++ {
+		s := g.r.Intn(cx / 2)
+		x := g.r.Intn(cx - s)
+		y := g.r.Intn(cy - s)
+
+		r := image.Rect(x, y, x+s, y+s)
+		draw.Draw(i.Image, r, c, image.ZP, draw.Src)
+
+		r = image.Rect(width-x-s, y, (width - x), y+s)
+		draw.Draw(i.Image, r, c, image.ZP, draw.Src)
+
+		r = image.Rect(x, height-y-s, x+s, (height - y))
+		draw.Draw(i.Image, r, c, image.ZP, draw.Src)
+
+		r = image.Rect(width-x-s, height-y-s, (width - x), (height - y))
+		draw.Draw(i.Image, r, c, image.ZP, draw.Src)
+	}
+
+	return i, nil
+}
+
+func init() {
+	g := &symsquareIconGen{
+		rand.New(rand.NewSource(time.Now().Unix())),
+		5,
+		[]color.Color{
+			color.RGBA{0xaa, 0x66, 0x66, 0xff},
+			color.RGBA{0x66, 0xaa, 0x66, 0xff},
+			color.RGBA{0x66, 0x66, 0xaa, 0xff},
+		},
+		image.NewUniform(color.RGBA{0xff, 0xff, 0xff, 0xff}),
+	}
+	Register("symsquare", g)
+}
---
+++ symsquare_test.go
@@ -0,0 +1,46 @@
+// Tests the basics of the framework on a concrete Generator.
+
+package ricons
+
+import "testing"
+
+func TestSymsquarePresent(t *testing.T) {
+	if gen, exist := Registry["symsquare"]; !exist {
+		t.Fatal("couldn't find 'symsquare' generator")
+	} else {
+		g = gen
+	}
+}
+
+func TestSymsquareStringer(t *testing.T) {
+	if g.String() != "symsquare: symmetric square-based icons" {
+		t.Errorf("stringer mismatch: %v != %v", "symsquare: symmetric square-based icons", g.String())
+		return
+	}
+}
+
+func BenchmarkSymsquare16x16(b *testing.B) {
+	g, ok := Registry["symsquare"]
+	if !ok {
+		b.Fatal("couldn't find 'symsquare' generator")
+	}
+	for i := 0; i < b.N; i++ {
+		_, err := g.NewIcon(16, 16)
+		if err != nil {
+			b.Fatalf("(%d) error generating icon: %s", i+1, err)
+		}
+	}
+}
+
+func BenchmarkSymsquare32x32(b *testing.B) {
+	g, ok := Registry["symsquare"]
+	if !ok {
+		b.Fatal("couldn't find 'symsquare' generator")
+	}
+	for i := 0; i < b.N; i++ {
+		_, err := g.NewIcon(32, 32)
+		if err != nil {
+			b.Fatalf("(%d) error generating icon: %s", i+1, err)
+		}
+	}
+}
