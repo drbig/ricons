2082f95dfc29826663dea2587a1e7cfcf19ec2e9
Piotr S. Staszewski <p.staszewski@gmail.com>
2014-12-08 13:05:19 +0100

    Fix generate.rb

--- showcase/generate.rb
+++ showcase/generate.rb
@@ -5,23 +5,28 @@
 require 'open-uri'
 require 'json'
 
+REGEN = false
 N = 4
 ROOT = 'http://127.0.0.1:3232'
-table = 'Generator|Size|' + 1.upto(N).to_a.join('|') + "\n" +\
-        '---------|----|' + ('-|' * (N-1)) + '-' + "\n"
+table = '| Generator | Size | ' + 1.upto(N).to_a.join(' | ') + " |\n" +\
+        '| --------- | ---- | ' + ('---- | ' * (N-1)) + "---- |\n"
 
 info = JSON.parse(open(ROOT + '/info.json').read, :symbolize_names => true)
 info[:generators].each_pair do |k,v|
   [16, 100].each do |s|
-    STDERR.puts "Fetching #{N} examples for #{v} of #{s}x#{s} size"
-    table += "#{k}|#{s}x#{s}|"
+    if REGEN 
+      STDERR.puts "Fetching #{N} examples for #{v} of #{s}x#{s} size"
+    end
+    table += "| #{k} | #{s}x#{s} | "
     tmp = []
     1.upto(N) do |i|
       fn = "#{k}-#{s}x#{s}-#{i}.png"
-      open(fn, 'wb') {|f| f.write(open(ROOT + "/#{k}/png/#{s}/#{s}").read) }
+      if REGEN
+        open(fn, 'wb') {|f| f.write(open(ROOT + "/#{k}/png/#{s}/#{s}").read) }
+      end
       tmp.push("![#{k} #{i}](https://raw.github.com/drbig/ricons/master/showcase/#{fn})")
     end
-    table += tmp.join('|') + "\n"
+    table += tmp.join(' | ') + " |\n"
   end
 end
 
